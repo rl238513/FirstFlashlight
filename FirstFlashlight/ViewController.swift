f774b03dcec6e8de1d989c105ffe545e9f8fd336
EDUARDO MENDOZA <rl238513@rlas-macair-36.rlas-116.org>
2018-12-11 10:51:21 -0600

    Initial Commit

---
+++ FirstFlashlight/ViewController.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController.swift
+//  FirstFlashlight
+//
+//  Created by EDUARDO MENDOZA on 8/28/18.
+//  Copyright © 2018 EDUARDO MENDOZA. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+  
+    @IBAction func onFlashColorButtonTapped(_ sender: Any) {
+   self.view.backgroundColor = .white
+    }
+    
+    
+}
